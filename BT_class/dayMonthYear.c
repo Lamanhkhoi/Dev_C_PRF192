1b3ae321a0e4259c7fcaf091ebae585b33d47dd5
Lâm Anh Khôi <lamanhkhoi06@gmail.com>
2025-05-23 08:41:54 +0700

    fixing code

--- BT_class/dayMonthYear.c
+++ BT_class/dayMonthYear.c
@@ -3,59 +3,59 @@
 int main(){
     int month, year, day;
     int totalDayOfMonth;
- 
-    do{
-        printf("Input year :=");
-        scanf("%d",&year);
-        printf("Input month :=");
-        scanf("%d",&month);
-        printf("Input day :=");
-        scanf("%d",&day);
-        if ( month < 1 || month > 12 || day < 1 || day > 31){
-            printf("ERROR !!! Please try again !!!\n");
+    int validInput;
+do {
+    validInput = 1; 
+    printf("Input year :="); scanf("%d", &year);
+    printf("Input month :="); scanf("%d", &month);
+    printf("Input day :="); scanf("%d", &day);
+
+    if (month < 1 || month > 12) {
+        printf("ERROR: Month must be between 1 and 12.\n");
+        validInput = 0;
+    } else {
+        switch (month) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                totalDayOfMonth = 31;
+                break;
+            case 4: case 6: case 9: case 11:
+                totalDayOfMonth = 30;
+                break;
+            case 2:
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                    totalDayOfMonth = 29;
+                } else {
+                    totalDayOfMonth = 28;
+                }
+                break;
         }
-    }while (month < 1 || month > 12 || day < 1 || day > 31);
 
-    switch (month){
-    case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-        totalDayOfMonth = 31;
-        break;
-    case 4: case 6: case 9: case 11:
-        totalDayOfMonth = 30;
-        break;
-    default:
-        if ( year % 4 == 0 || year % 400 == 0 ){
-            totalDayOfMonth = 29;
-        }else {
-            totalDayOfMonth = 28;
-        } 
-        break;
+        if (validInput && (day < 1 || day > totalDayOfMonth)) { 
+            printf("ERROR: Day %d is invalid for month %d, year %d (should be 1-%d).\n", day, month, year, totalDayOfMonth);
+            validInput = 0;
+        }
     }
 
-    printf("Day of month %d/ year %d is: %d\n", month, year, totalDayOfMonth);
-    
+    if (!validInput) {
+        printf("Please try again !!!\n");
+    }
+} while (!validInput);
+
+printf("Day of month %d/ year %d is: %d\n", month, year, totalDayOfMonth);
+
     // check day
-    if (month == 12 && day == 31){
-        month -= 11;
-        day = 1;
-        year += 1;
-    }else if ( month == 2){
-        if ( totalDayOfMonth == 28 || totalDayOfMonth == 29){
-            day = 1;
-            month += 1;
-        }else{day += 1;}
-    } else if ( month % 2 == 0 && month != 8 && month != 2){
-        if ( day == 30){
-            day = 1;
-            month += 1;
-        }else{day += 1;}
-    }else {
-        if ( day == 31){
-            day = 1;
-            month += 1;
-        }else{day += 1;}
+    if (day < totalDayOfMonth) {
+        day++;
+    } else {
+        day = 1; 
+        if (month < 12) {
+            month++;
+        } else {
+            month = 1; 
+            year++;    
+        }
     }
 
-    printf("Current time today is: %d/%d/%d", day,month,year);
+    printf("Current time tomorrow is: %d/%d/%d\n", day, month, year);
     return 0;
 }
