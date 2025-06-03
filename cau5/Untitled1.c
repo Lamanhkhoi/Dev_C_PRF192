806c8cc053a61da716c03e1284c76b28343ca782
Lâm Anh Khôi <lamanhkhoi06@gmail.com>
2025-06-03 23:49:34 +0700

    fixing

--- cau5/Untitled1.c
+++ cau5/Untitled1.c
@@ -5,7 +5,6 @@ int main() {
     int array[4]; 
     int num_elements = 4; 
 
-<<<<<<< Updated upstream
     // Input numbers
     for (int i = 0; i < num_elements; i++) {
         printf("Input number %d: ", i + 1);
@@ -34,26 +33,4 @@ int main() {
     printf("The smallest number := %d\n", min);
 
     return 0;
-=======
-int main(){
-	int min, max;
-	int array[1000];
-	
-	for ( int i = 0; i < 4; i++ ){
-		printf("Input number :=");
-		scanf("%d", &array[i]);
-	}
-	
-	for ( int i = 0; i < 4; i++ ){
-		if ( array[i] > array[i+1] ){
-			max = array[i];
-		} else if ( array[i] < array[i+1] ){
-			min = array[i];
-		}
-	}
-	
-	printf("The largest number := %d\n", max);
-	printf("The smallest number := %d", min);
-	return 0;
->>>>>>> Stashed changes
 }
