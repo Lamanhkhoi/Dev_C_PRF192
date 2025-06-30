b8598c161afdd552cc3c5dbac499eaa7132485e3
khoi-3 <lamanhkhoi06@gmail.com>
2025-06-30 16:48:53 +0700

    add new code

--- BT_class/Demo_code/testOOP2.c
+++ BT_class/Demo_code/testOOP2.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define max 50
 
-typeof struct date{
+typedef struct {
 	int id;
 	char name[20];
 	float grade;
 	
 }student;
 
-void inputStudentInfo(student *s);
-void printAStudent(student *s)
+void printAStudent(student s);
+void inputStudent(student a[], int n);
 void printStudent(student a[], int n);
-void printAddress(student *s);
 int main(){
 	student a[max];
 	int n;
-	printf("How many student? ";
-	scan("%d", &n);
-	
+	printf("How many student? ");
+	scanf("%d", &n);
+	inputStudent(a,n);
+	printf("\nstudent list:\n");
+	printStudent(a,n);
+	return 1;
+}
+
+void printAStudent(student s){
+	printf("%d - %s - %.1f\n",s.id,s.name,s.grade);
+}
+
+void inputStudent(student a[], int n){
+	for (int i = 0; i < n; i++){
+		printf("id = ");scanf("%d",&a[i].id);
+		fflush(stdin);
+		printf("name = ");scanf("[^\n]",&a[i].name);
+		printf("grade = ");scanf("%f",&a[i].grade);
+	}
+}
+
+void printStudent(student a[], int n){
+	for (int i = 0; i < n; i++){
+		printAStudent(a[i]);
+	}
 }
