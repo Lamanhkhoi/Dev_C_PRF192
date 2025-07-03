4bc43846586f5a27c8c78b15eaf6ac0c8050ebe9
khoi-3 <lamanhkhoi06@gmail.com>
2025-07-03 09:54:59 +0700

    add new code

---
+++ BT_class/Demo_code/string01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype>
+
+int main(){
+	char a[21];
+	char b[] = "Hello World!";
+	char c[31] = {'H','i','\0'};
+	int i,n = strlen(a);
+	strcpy(a,"Hello");
+	printf("%s", a);
+	printf("len = %d",n);
+	printf("/nCach 2:" );
+	for ( int i = 0; str)
+}
